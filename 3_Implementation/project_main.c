5a2cdb4c6da2a27992009306ce88f49b2ebf9f72
SANTHOSH-1999 <94223235+SANTHOSH-1999@users.noreply.github.com>
2021-11-24 00:23:52 +0530

    Update project_main.c

--- 3_Implementation/project_main.c
+++ 3_Implementation/project_main.c
@@ -1,1 +1,30 @@
+#include <employee_management.h>
+typedef struct
+{
+    int yyyy;
+    int mm;
+    int dd;
+} Date;
+typedef struct
+{
+    char username[MAX_SIZE_USER_NAME];
+    char password[MAX_SIZE_PASSWORD];
+} sFileHeader;
+//Elements of structure
+typedef struct// to call in program
+{
+    char fatherName[MAX_FATHER_NAME];// declare the character data type
+    char employeeName[MAX_EMPLOYEE_NAME];// declare the character data type
+    char employeeAddr[MAX_EMPLOYEE_ADDRESS];// declare the character data type
+    Date employeeJoiningDate;// declare the integer data type
+    unsigned int employee_id; // declare the integer data type
+    float employeeSalary;
+} s_EmployeesInfo;
 
+int main()
+{
+    init();
+    welcomeMessage();
+    login();
+    return 0;
+}
