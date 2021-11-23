21cdbe56fbae83a3cc178b3487197c79901c3a3a
SANTHOSH-1999 <94223235+SANTHOSH-1999@users.noreply.github.com>
2021-11-23 23:38:29 +0530

    Update employee_management.h

--- 3_Implementation/inc/employee_management.h
+++ 3_Implementation/inc/employee_management.h
@@ -18,13 +18,3 @@ void menu();
 void login();
 void init();
 int isFileExists(const char *path);
-#define MAX_YR  9999
-#define MIN_YR  1900
-#define MAX_SIZE_USER_NAME 30
-#define MAX_SIZE_PASSWORD  20
-#define FILE_NAME  "EmployeeRecordSystem.bin"
-// Macro related to the Employees info
-#define MAX_FATHER_NAME 50
-#define MAX_EMPLOYEE_NAME 50
-#define MAX_EMPLOYEE_ADDRESS 300
-#define FILE_HEADER_SIZE  sizeof(sFileHeader)
